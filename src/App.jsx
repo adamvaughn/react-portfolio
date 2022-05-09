c2d681fed89445233148d03b21e1bf181501cc4e
adamvaughn <adamdvaughn@gmail.com>
2022-05-09 01:48:06 -0500

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import Intro from "./components/intro/intro"
-import About from "./components/about/About"
-import ProjectList from "./components/projectList/ProjectList"
-import Contact from "./components/contact/Contact"
+import { useContext } from "react";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/intro";
+import ProductList from "./components/projectList/ProjectList";
+import Toggle from "./components/toggle/Toggle";
+import { ThemeContext } from "./context";
 
 const App = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: darkMode ? "#222" : "white",
+        color: darkMode && "white",
+      }}
+    >
+      <Toggle />
       <Intro />
       <About />
-      <ProjectList />
+      <ProductList />
       <Contact />
     </div>
-  )
-}
+  );
+};
 
 export default App;
